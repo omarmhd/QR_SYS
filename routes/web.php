236d5651c6b23f9c2daed9147b8811a102bbf36e
omarmhd <omarmhd19988@gmail.com>
2025-07-15 01:40:45 +0300

    plans,users,onboarding screen

--- routes/web.php
+++ routes/web.php
@@ -6,17 +6,15 @@ use App\Http\Controllers\ProfileController;
 use App\Models\OnboardingScreen;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+
+// Route::get('/', function () {
+//     return view('dashboard');
+// })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
 
-    Route::get("requests",[MembershipRequests::class,'index'])->name("requests.index");
+    Route::get("/",[MembershipRequests::class,'index'])->name("requests.index");
     Route::get('/requests/{id}/change-status/{status}', [MembershipRequests::class, 'changeStatus'])
     ->name('requests.changeStatus');
 
