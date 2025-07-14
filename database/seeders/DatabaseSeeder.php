839fff16ef6b0214e3147fdb045636e90bfa4112
omarmhd <omarmhd19988@gmail.com>
2025-07-15 01:35:34 +0300

    plans,users,onboarding screen

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,12 +18,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-            // Admin::create([
-            // "name"=>"admin",
-            // "password"=>Hash::make("admin123"),
-            // "email"=>"admin@admin.com",
-            // "role"=>"administrator"]);
+        User::factory(10)->create();
+            Admin::create([
+            "name"=>"admin",
+            "password"=>Hash::make("admin123"),
+            "email"=>"admin@admin.com",
+            "role"=>"administrator"]);
                        Plan::factory()->count(10)->create();
 
 
