b2f6e4ee69e8375af4fa6de3546bc2288e98e993
omarmhd <omarmhd19988@gmail.com>
2025-09-13 21:37:26 +0300

    some change in lounges

--- resources/views/lounges/index.blade.php
+++ resources/views/lounges/index.blade.php
@@ -112,7 +112,7 @@
                                             <td>{{$lounge->excerpt["en"]}} </td>
                                             <td>
                                                 {{-- Edit Button --}}
-                                                <a href="{{ route('lounges.edit', $lounge->id) }}" class="btn btn-bg btn-primary">
+                                                <a href="{{ route('lounges.edit', $lounge->id) }}" class="btn btn-icon  bg-blue text-white ">
                                                     <svg xmlns="http://www.w3.org/2000/svg" width="25" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-edit">
                                                         <path stroke="none" d="M0 0h24v24H0z" fill="none" />
                                                         <path d="M7 7h-1a2 2 0 0 0 -2 2v9a2 2 0 0 0 2 2h9a2 2 0 0 0 2 -2v-1" />
@@ -123,7 +123,7 @@
                                                 <form action="{{ route('lounges.destroy', $lounge->id) }}" method="POST" style="display:inline;" class="delete-form">
                                                     @csrf
                                                     @method('DELETE')
-                                                    <button type="submit" class="btn btn-bg btn-danger delete-btn">
+                                                    <button type="submit" class="btn btn-icon  bg-red text-white  delete-btn">
                                                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-trash">
                                                             <path stroke="none" d="M0 0h24v24H0z" fill="none" />
                                                             <path d="M4 7l16 0" />
