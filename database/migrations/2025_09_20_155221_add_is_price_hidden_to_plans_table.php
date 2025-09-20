f5dc09eb4be856ba70b97c4dfc4bb0ba06672b01
omarmhd <omarmhd19988@gmail.com>
2025-09-20 21:31:51 +0300

    updates

--- database/migrations/2025_09_20_155221_add_is_price_hidden_to_plans_table.php
+++ database/migrations/2025_09_20_155221_add_is_price_hidden_to_plans_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('plans', function (Blueprint $table) {
-            $table->boolean('is_price_hidden')->default(fasle)->after('price');
+            $table->boolean('is_price_hidden')->default(false)->after('price');
 
         });
     }
