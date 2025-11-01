da586b46bbcf02d0138305034ac9181c3fdb031f
omarmhd <omarmhd19988@gmail.com>
2025-11-01 22:27:02 +0200

    add notes

--- app/Http/Controllers/PlanController.php
+++ app/Http/Controllers/PlanController.php
@@ -29,7 +29,7 @@ class PlanController extends Controller
         'name.*' => 'required',
          'price' => 'required_if:is_price_hidden,0|numeric|nullable',
         'guest_passes_per_year' => 'required|integer',
-        'currency' => 'required|in:EUR,RO',
+        'currency' => 'required|in:EUR,RON',
         'billing_type' => 'required|in:day,month,year',
         'features' => 'required|array|min:1',
         'features.*' => 'required',
