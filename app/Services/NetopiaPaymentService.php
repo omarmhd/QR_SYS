77d50e25a97cd52b0762d45337e4a096e36b80f7
omarmhd <omarmhd19988@gmail.com>
2025-11-21 16:19:32 +0200

    test

--- app/Services/NetopiaPaymentService.php
+++ app/Services/NetopiaPaymentService.php
@@ -8,6 +8,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Mail;
 
 class NetopiaPaymentService
 {
